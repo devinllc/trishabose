9275ca90d8e1546c7b02cc152dc7470bd87cd369
Ramesh Vishwakarma <114978317+devinllc@users.noreply.github.com>
2025-03-08 15:35:05 +0530

    testmonials

--- script.js
+++ script.js
@@ -39,3 +39,76 @@ document.addEventListener('scroll', function () {
         document.querySelector(`#section${currentSection}`).classList.add('show');
     }
 });
+
+
+// Sub-category data for each category
+const subCategories = {
+    photoshoot: [
+        'Garments',
+        'Lifestyle',
+        'BrandShoots',
+        'Fashion',
+        'Portrait',
+        'Staycations'
+    ],
+    acting: [
+        'Music Videos',
+        'Films',
+        'Shortfilms',
+        'Series',
+        'Shows',
+        'Advertisements'
+    ],
+    influencer: [
+        'Promotions',
+        'Paid Collabs'
+    ]
+};
+
+// Function to populate the sub-categories dropdown based on the selected category
+function populateSubCategories() {
+    const categorySelect = document.getElementById('category');
+    const subCategorySelect = document.getElementById('sub-category');
+    const selectedCategory = categorySelect.value;
+
+    // Clear current sub-category options
+    subCategorySelect.innerHTML = '<option value="">Select Sub-Category</option>';
+
+    // If a valid category is selected, populate sub-category options
+    if (selectedCategory && subCategories[selectedCategory]) {
+        const subCategoryOptions = subCategories[selectedCategory];
+        subCategoryOptions.forEach(function (subCat) {
+            const option = document.createElement('option');
+            option.value = subCat.toLowerCase().replace(/\s+/g, '-');
+            option.text = subCat;
+            subCategorySelect.appendChild(option);
+        });
+    }
+}
+
+
+
+// gallleriersssss
+
+// Get the modal
+var modal = document.getElementById("imageModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modalImg = document.getElementById("modalImage");
+var captionText = document.getElementById("caption");
+
+document.querySelectorAll(".project-image").forEach(image => {
+    image.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.dataset.full;
+        captionText.innerHTML = this.alt;
+    }
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
